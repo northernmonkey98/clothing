977664eaaf777989847c1119d744aeadda8349f2
novamonkey <32420319+northernmonkey98@users.noreply.github.com>
2025-10-11 02:23:29 +0100

    base clothing for gov

---
+++ cfx_onx_gov_clothing/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'ONX'
+contributors 'CanisterDog, Momo'
+description 'Government, Police & EMS Clothing'
+version '1.4.0'
+lua54 'yes'
+
+server_scripts {
+  'version.lua',
+}
+
+files {
+  'mp_m_freemode_01_onxgovt.meta',
+  'mp_f_freemode_01_onxgovt.meta',
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_onxgovt.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_onxgovt.meta'
+dependency '/assetpacks'
